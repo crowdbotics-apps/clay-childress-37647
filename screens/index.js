134a9d36c810293c83986f54041e6d02d60cd3cf
Crowdbotics <team@crowdbotics.com>
2022-12-08 00:18:09 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,89 +1,95 @@
 import React, { useState, useEffect } from "react";
 import {
   Text,
-  View,
   StyleSheet,
-  Pressable,
-  TextInput,
+  View,
   Image,
-  ScrollView
+  ScrollView,
+  TextInput
 } from "react-native";
 
-const Feedback = () => {
+const DirectMessagesScreen = (params) => {
   const [message, setMessage] = useState("");
-  const [reviews, setReviews] = useState([]);
-  const [expanded, setExpanded] = useState(null);
+  const [user1, setUser1] = useState({});
+  const [user2, setUser2] = useState({});
+  const [conversation, setConversation] = useState([]);
   useEffect(() => {
-    setReviews([
+    setUser1({
+      name: "User1",
+      image: require("./assets/profile1.png"),
+      isOnline: true
+    });
+    setUser2({
+      name: "Username",
+      image: require("./assets/profile2.png"),
+      isOnline: true
+    });
+  }, []);
+  useEffect(() => {
+    setConversation([
       {
         id: 1,
-        username: "Username",
-        email: "username@email.com",
-        image: require("./assets/userImage.png"),
-        subject: "Molestie vestibulum nulla.",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pretium, commodo lacus, amet nulla faucibus vulputate erat vestibulum. Aliquet consequat nunc sit ullamcorper vel egestas nunc sagittis lectus. Sed ipsum vel in morbi non semper adipiscing nibh nam. Integer sem."
+        sender: user1,
+        text: "lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        sentTime: "12:00 PM"
       },
       {
         id: 2,
-        username: "Username",
-        email: "username@email.com",
-        image: require("./assets/userImage.png"),
-        subject: "Molestie vestibulum nulla.",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pretium, commodo lacus, amet nulla faucibus vulputate erat vestibulum. Aliquet consequat nunc sit ullamcorper vel egestas nunc sagittis lectus. Sed ipsum vel in morbi non semper adipiscing nibh nam. Integer sem."
+        sender: user2,
+        text: "lorem ipsum dolor sit amet",
+        sentTime: "01:15 PM"
       },
       {
         id: 3,
-        username: "Username",
-        email: "username@email.com",
-        image: require("./assets/userImage.png"),
-        subject: "Molestie vestibulum nulla.",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pretium, commodo lacus, amet nulla faucibus vulputate erat vestibulum. Aliquet consequat nunc sit ullamcorper vel egestas nunc sagittis lectus. Sed ipsum vel in morbi non semper adipiscing nibh nam. Integer sem."
+        sender: user2,
+        text: "lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        sentTime: "01:15 PM"
       },
       {
         id: 4,
-        username: "Username",
-        email: "username@email.com",
-        image: require("./assets/userImage.png"),
-        subject: "Molestie vestibulum nulla.",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pretium, commodo lacus, amet nulla faucibus vulputate erat vestibulum. Aliquet consequat nunc sit ullamcorper vel egestas nunc sagittis lectus. Sed ipsum vel in morbi non semper adipiscing nibh nam. Integer sem."
+        sender: user1,
+        text: "lorem ipsum dolor sit amet",
+        sentTime: "01:15 PM"
+      },
+      {
+        id: 5,
+        sender: user2,
+        text: "lorem ipsum dolor sit amet",
+        sentTime: "01:15 PM"
+      },
+      {
+        id: 6,
+        sender: user1,
+        text: "lorem ipsum dolor sit amet",
+        sentTime: "01:15 PM"
       }
     ]);
-  }, []);
-  useEffect(() => {
-    setExpanded(reviews[3]);
-  }, [reviews]);
-  const handleExpand = item => {
-    if (expanded === item) {
-      setExpanded(null);
-    } else {
-      setExpanded(item);
-    }
-  };
+  }, [user1, user2]);
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <TabView
-          tabTitles={["All Users"]}
-          selected={0}
-          style={styles.tabView}
-        />
-        <Text style={styles.title}>Feedback</Text>
-        <View style={styles.feedbackContainer}>
-          {reviews.map((review, index) => (
-            <Review
-              key={index}
-              item={review}
-              expanded={review === expanded}
-              onPress={x => {
-                handleExpand(x);
-              }}
+      <View style={styles.header}>
+        <View style={styles.headerImage}>
+          <Image source={user2.image} style={styles.headerImage} />
+          {(user2.isOnline && (
+            <Image
+              source={require("./assets/onlineIcon.png")}
+              style={styles.activityIconHeader}
             />
-          ))}
+          )) ||
+            null}
         </View>
+        <View style={styles.profileName}>
+          <Text style={styles.profileNameText}>{user2.name}</Text>
+        </View>
+        <View style={styles.icons}>
+          <Image source={require("./assets/phoneIcon.png")} />
+          <Image source={require("./assets/videoIcon.png")} />
+        </View>
+      </View>
+      <ScrollView style={styles.body}>
+        {conversation.map((message, index) => (
+          <ConversationElement message={message} key={index} />
+        ))}
       </ScrollView>
       <View style={styles.footer}>
         <View style={styles.camera}>
@@ -96,7 +102,7 @@ const Feedback = () => {
           <TextInput
             style={styles.input}
             placeholder="Type a message"
-            onChangeText={text => setMessage(text)}
+            onChangeText={(text) => setMessage(text)}
             value={message}
             autoCorrect={false}
             autoCapitalize="none"
@@ -122,128 +128,53 @@ const Feedback = () => {
   );
 };
 
-const Review = ({ item, expanded, onPress }) => {
-  return (
-    <Pressable style={reviewStyles.container} onPress={() => onPress(item)}>
-      <View style={reviewStyles.header}>
-        <Image source={item.image} style={reviewStyles.image} />
-        <View style={reviewStyles.info}>
-          <Text style={reviewStyles.username}>{item.username}</Text>
-          <Text style={reviewStyles.email}>{item.email}</Text>
-        </View>
-        <Image
-          source={require("./assets/menuIcon.png")}
-          style={reviewStyles.menuIcon}
-        />
-      </View>
-      {expanded
-        ? (
-        <View style={reviewStyles.body}>
-          <Text style={reviewStyles.detailsText}>
-            <Text style={reviewStyles.green}>Email: {"\t"}</Text>
-            {"\t"}
-            {item.email}
-          </Text>
-          <Text style={reviewStyles.detailsText}>
-            <Text style={reviewStyles.green}>Subject: </Text>
-            {"\t"}
-            {item.subject}
-          </Text>
-          <Text style={reviewStyles.description}>{item.description}</Text>
-          <Pressable style={reviewStyles.btn}>
-            <Text style={reviewStyles.btnText}>Reply</Text>
-          </Pressable>
-        </View>
-          )
-        : null}
-    </Pressable>
-  );
-};
-
-const reviewStyles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    backgroundColor: "#f1f1f1",
-    borderRadius: 10,
-    marginHorizontal: 20,
-    marginBottom: 10
-  },
-  header: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between"
-  },
-  image: {
-    width: 50,
-    height: 50,
-    borderRadius: 25
-  },
-  menuIcon: {
-    width: 20,
-    height: 20
-  },
-  username: {
-    fontSize: 14
-  },
-  email: {
-    fontSize: 12
-  },
-  info: {
-    flexDirection: "column",
-    flex: 1,
-    justifyContent: "space-between",
-    alignItems: "flex-start",
-    marginHorizontal: 10,
-    height: 40
-  },
-  body: {
-    marginTop: 10,
-    paddingTop: 10,
-    borderTopWidth: 1,
-    borderTopColor: "#ccc"
-  },
-  detailsText: {
-    marginBottom: 5
-  },
-  green: {
-    color: "#12D790"
-  },
-  description: {
-    fontSize: 12,
-    marginTop: 5,
-    textAlign: "justify"
-  },
-  btn: {
-    backgroundColor: "#000",
-    width: 60,
-    height: 30,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 10,
-    marginVertical: 5,
-    alignSelf: "flex-end"
-  },
-  btnText: {
-    color: "#fff",
-    fontSize: 12
-  }
-});
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff"
   },
-  tabView: {
-    width: 150,
-    marginLeft: 20,
-    marginVertical: 10
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+    marginHorizontal: 20,
+    backgroundColor: "#F1F1F1",
+    borderRadius: 10,
+    height: 50
   },
-  title: {
-    marginLeft: 20,
-    marginBottom: 10,
-    marginTop: 20
+  headerImage: {
+    flex: 1,
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    resizeMode: "contain",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  profileName: {
+    flex: 2
+  },
+  profileNameText: {
+    fontSize: 16
+  },
+  icons: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around"
+  },
+  activityIconHeader: {
+    width: 10,
+    height: 10,
+    resizeMode: "contain",
+    position: "absolute",
+    right: 20,
+    top: 25
+  },
+  body: {
+    flex: 1,
+    marginTop: 10
   },
   footer: {
     flexDirection: "row",
@@ -258,8 +189,7 @@ const styles = StyleSheet.create({
   input: {
     paddingLeft: 15,
     borderRadius: 10,
-    backgroundColor: "#F1F1F1",
-    height: 45
+    backgroundColor: "#F1F1F1"
   },
   smileyIcon: {
     position: "absolute",
@@ -268,76 +198,89 @@ const styles = StyleSheet.create({
     opacity: 0.5
   },
   voiceIcon: {
-    top: 12,
+    top: 14,
     right: 15,
     position: "absolute",
     opacity: 0.5
   }
 });
+export default DirectMessagesScreen;
 
-export default Feedback;
-
-const TabView = ({
-  tabTitles,
-  selected,
-  onPress,
-  tabColor,
-  backgroundColor,
-  style
-}) => {
-  const tabColorStyle = {
-    backgroundColor: tabColor || "#fff"
+const ConversationElement = ({ message }) => {
+  const boxAlignment = {
+    flexDirection: message.sender.name === "User1" ? "row" : "row-reverse"
   };
-  const backgroundColorStyle = {
-    backgroundColor: backgroundColor || "#F1F1F1"
+  const messageTextContainer = {
+    marginLeft: message.sender.name === "User1" ? 20 : 0,
+    marginRight: message.sender.name === "User1" ? 0 : 20,
+    backgroundColor: message.sender.name === "User1" ? "#FCF1D6" : "#F9D8D9"
   };
-  const propStyle = style || {};
   return (
-    <View
-      style={[tabViewStyles.paletteContainer, backgroundColorStyle, propStyle]}>
-      {tabTitles.map((title, index) => (
-        <Pressable
-          onPress={() => (onPress ? onPress(index) : null)}
-          style={
-            index === selected
-              ? [tabViewStyles.selected, tabColorStyle]
-              : [tabViewStyles.unSelected, backgroundColorStyle]
-          }
-          key={index}>
-          <Text>{title}</Text>
-        </Pressable>
-      ))}
+    <View style={[ConversationElementStyles.messageContainer, boxAlignment]}>
+      <View style={ConversationElementStyles.profileImageContainer}>
+        <Image
+          source={message.sender.image}
+          style={ConversationElementStyles.profileImage}
+        />
+        {(message.sender.isOnline && (
+          <Image
+            source={require("./assets/onlineIcon.png")}
+            style={ConversationElementStyles.activityIcon}
+          />
+        )) ||
+          null}
+      </View>
+      <View
+        style={[
+          ConversationElementStyles.messageTextContainer,
+          messageTextContainer
+        ]}
+      >
+        <Text style={ConversationElementStyles.messageText}>
+          {message.text}
+        </Text>
+        <Text style={ConversationElementStyles.messageTime}>
+          {message.sentTime}
+        </Text>
+      </View>
     </View>
   );
 };
 
-const tabViewStyles = StyleSheet.create({
-  paletteContainer: {
-    width: "80%",
-    height: 48,
-    backgroundColor: "#E4E4E4",
+const ConversationElementStyles = StyleSheet.create({
+  messageContainer: {
     flexDirection: "row",
     alignItems: "center",
-    borderRadius: 10,
-    padding: 6,
-    marginVertical: 10
+    margin: 10,
+    justifyContent: "flex-start",
+    marginVertical: 20
   },
-  selected: {
+  messageTextContainer: {
+    minHeight: 70,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
     borderRadius: 10,
-    flex: 1,
-    backgroundColor: "#fff",
-    height: "100%",
     justifyContent: "center",
-    alignItems: "center",
-    shadowColor: "gray",
-    elevation: 10
+    alignItems: "flex-start",
+    width: "70%"
   },
-  unSelected: {
-    flex: 1,
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#E4E4E4",
-    borderRadius: 10
+  messageText: {
+    lineHeight: 20,
+    fontSize: 14,
+    color: "#000",
+    fontWeight: "bold",
+    textAlign: "left"
+  },
+  activityIcon: {
+    position: "absolute",
+    right: 0,
+    top: 40
+  },
+  messageTime: {
+    position: "absolute",
+    right: 5,
+    bottom: -20,
+    color: "grey",
+    fontSize: 12
   }
 });
